93c2ffe8194ea002bf26796638911859c2a93a9a
Student <jsitbon@qcc.cuny.edu>
2019-07-25 15:37:04 -0400

    employee

---
+++ client/src/employee/employee.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import axios from 'Axios';
+
+class Employee extends Component {
+    constructor() {
+        super();
+        this.state = {
+            employees: []
+        }
+    }
+
+    // put methods here
+
+    render (){
+        <div></div>
+
+        }
+    }
+    
+}
+
+export default Employee
